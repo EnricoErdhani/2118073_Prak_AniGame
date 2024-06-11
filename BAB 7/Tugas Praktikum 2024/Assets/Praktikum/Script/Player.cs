2d67dd917e853a6c8b468dd4d3300673958af742
EnricoErdhani <2118073@scholar.itn.ac.id>
2024-06-11 21:30:16 +0700

    BAB 10

--- BAB 7/Tugas Praktikum 2024/Assets/Praktikum/Script/Player.cs
+++ BAB 7/Tugas Praktikum 2024/Assets/Praktikum/Script/Player.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
      Rigidbody2D rb;
      public Animator animator;
+     public GameObject bullet;
+     public Transform firePoint;
+     public int nyawa;
+[SerializeField] Vector3 respawn_loc;
+public bool play_again;
+
 
   [SerializeField] Transform groundcheckCollider;
   [SerializeField] LayerMask groundLayer;
 
-  const float groundCheckRadius = 0.2f; // +
+  const float groundCheckRadius = -0.49f; // +
   [SerializeField] float speed = 1;
   [SerializeField] float jumpPower = 100;
   
@@ -24,10 +31,25 @@ public class Player : MonoBehaviour
   {
     rb = GetComponent<Rigidbody2D>();
     animator = GetComponent<Animator>();
+    respawn_loc = transform.position;
   }
+
+void playagain()
+{
+  if(play_again == true)
+  {
+     nyawa = 3;
+     transform.position = respawn_loc;
+     play_again = false;
+  }
+}
 
   void Update ()
   {
+    if (Input.GetKeyDown(KeyCode.C))
+{
+    StartCoroutine(Attack());
+}
     horizontalValue = Input.GetAxisRaw("Horizontal");
     if (Input.GetButtonDown("Jump"))
     {
@@ -36,15 +58,37 @@ public class Player : MonoBehaviour
     }
     else if (Input.GetButtonUp("Jump"))
     jump = false;
+    if (nyawa < 0)
+{
+  playagain();
+}
+
+if(transform.position.y < -10)
+{
+  play_again = true;
+  playagain();
+}
   }
 
    void FixedUpdate()
+   
   {
   GroundCheck();
   Move(horizontalValue, jump);
   animator.SetFloat("Blend", Mathf.Abs(rb.velocity.x));
   animator.SetFloat("Blend Jump", rb.velocity.y);
   }
+  IEnumerator Attack()
+{
+     animator.SetTrigger("Attack");
+     yield return new WaitForSeconds(0.25f);
+     float direction = facingRight? 1f:-1f;
+
+     GameObject fireball = Instantiate(bullet, firePoint.position, Quaternion.identity);
+     fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(direction * 10f, 0);
+     Destroy(fireball, 2f);
+}
+
 
   void GroundCheck()
   {
